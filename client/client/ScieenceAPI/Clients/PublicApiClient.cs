e7789cece0e55fb7a77e3df99e594219c23a5693
Sviatoslav Chapcha <whitetark@gmail.com>
2022-11-12 01:31:46 +0200

    Initial commit

---
+++ client/client/ScieenceAPI/Clients/PublicApiClient.cs
@@ -0,0 +1,18 @@
+﻿namespace ScieenceAPI.Clients
+{
+    public class PublicApiClient
+    {
+        private HttpClient _client;
+        private static string _baseUrl;
+        private static string _apiKey;
+
+        public PublicApiClient()
+        {
+            _baseUrl = "http://api.springernature.com";
+            _apiKey = "eb9ca71479a5ebe84088018aad8881a2";
+
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri(_baseUrl);
+        }
+    }
+}
---
+++ client/client/ScieenceAPI/Models/Publication.cs
@@ -0,0 +1,38 @@
+﻿namespace ScieenceAPI.Models
+{
+    public class Publication
+    {
+        public Results result { get; set; }
+        public Records records { get; set; }
+    }
+
+    public class Results
+    {
+        public int total { get; set; }
+        public int start { get; set; }
+    }
+
+    public class Records
+    {
+        public string contentType { get; set; }
+        public string language { get; set; }
+        public Url url { get; set; }
+        public string title { get; set; }
+        public Creators[] creators { get; set; }
+        public string publicationName { get; set; }
+        public bool openaccess { get; set; }
+        public string publicationDate { get; set; }
+        public string publicationType { get; set; }
+        public string genre { get; set; }
+    }
+
+    public class Creators
+    {
+        public string creator { get; set; }
+    }
+
+    public class Url
+    {
+        public string value { get; set; }
+    }
+}
