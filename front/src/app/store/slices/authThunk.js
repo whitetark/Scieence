76c311f0e829ff1e2f097d2d69dde492098ada02
Sviatoslav Chapcha <whitetark@gmail.com>
2023-10-12 18:15:10 +0300

    small fixes

--- front/src/app/store/slices/authThunk.js
+++ front/src/app/store/slices/authThunk.js
@@ -3,25 +3,28 @@ import { getToken, removeToken, setToken } from '../../utils/HelperFunctions';
 import api from '../../services/api';
 import history from '../utils/history';
 
-export const fetchUserData = createAsyncThunk('Acc/getById', async (_, { rejectWithValue }) => {
-  try {
-    const accessToken = getToken();
-    api.defaults.headers.Authorization = `Bearer ${accessToken}`;
-    const response = await api.get('/getById');
-    return { ...response.data, accessToken };
-  } catch (e) {
-    removeToken();
-    return rejectWithValue('');
-  }
-});
+export const fetchUserData = createAsyncThunk(
+  'auth/fetchUserData',
+  async (_, { rejectWithValue }) => {
+    try {
+      const accessToken = getToken();
+      api.defaults.headers.Authorization = `Bearer ${accessToken}`;
+      const response = await api.get('/getById');
+      return { ...response.data, accessToken };
+    } catch (e) {
+      removeToken();
+      return rejectWithValue('');
+    }
+  },
+);
 
-export const login = createAsyncThunk('Acc/auth', async (payload) => {
+export const login = createAsyncThunk('auth/login', async (payload) => {
   const response = await api.post('/auth', payload);
   setToken(response.data.accessToken);
-  history.push('/home');
+  history.push('/');
   return response.data;
 });
 
-export const signOut = createAsyncThunk('Acc/signOut', async () => {
+export const signOut = createAsyncThunk('auth/signOut', async () => {
   removeToken();
 });
